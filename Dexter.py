f31e773330ad61286c8f2a2eba655eba5d7d388d
Kareem Sanchez <kareem.s56@Kareems-MacBook-Air.local>
2024-11-26 20:58:20 -0600

    Added a comment to Dexter.py

--- Dexter.py
+++ Dexter.py
@@ -2,12 +2,13 @@ import tkinter
 import pygame
 import pygame.mixer
 
+
 ROWS = 25
 COLS = 25
-TITLE_SIZE = 25
+TILE_SIZE = 25
 
-WINDOW_WIDTH = TITLE_SIZE * ROWS
-WINDOW_HEIGHT = TITLE_SIZE * COLS
+WINDOW_WIDTH = TILE_SIZE * ROWS
+WINDOW_HEIGHT = TILE_SIZE * COLS
 
 
 window = tkinter.Tk()
@@ -26,4 +27,48 @@ pygame.mixer.music.play()
 
 
 
+
+
+
+
+dexter = tkinter.PhotoImage(file="Dex.png")
+dexter_x = 0
+dexter_y = 0
+
+doakes = tkinter.PhotoImage(file="Doakes.png")
+doakes_x = 0
+doakes_y = 0
+
+def move(e):
+    global dexter_x, dexter_y
+    
+    if(e.keysym == "Up"):
+        dexter_y = dexter_y - TILE_SIZE
+
+    if(e.keysym == "Down"):
+        dexter_y = dexter_y + TILE_SIZE
+
+    if(e.keysym == "Left"):
+        dexter_x = dexter_x - TILE_SIZE
+
+    if(e.keysym == "Right"):
+        dexter_x = dexter_x + TILE_SIZE
+
+    
+    
+        
+
+
+
+def draw():
+    global dexter
+    canvas.delete("all")
+    canvas.create_image(dexter_x + (5 * TILE_SIZE) , dexter_y + (5 * TILE_SIZE), image=dexter)
+    canvas.create_image(doakes_x + (23 * TILE_SIZE) , doakes_y + (12 * TILE_SIZE), image=doakes)
+    window.after(100, draw)
+
+
+
+draw()
+window.bind("<KeyPress>", move)
 window.mainloop()
