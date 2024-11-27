68b504664a3519b3835ccdcda7ee6b68e038a295
Kareem Sanchez <kareem.s56@Kareems-MacBook-Air.local>
2024-11-26 18:10:06 -0600

    Added a comment to Dexter.py

--- Dexter.py
+++ Dexter.py
@@ -14,11 +14,10 @@ window = tkinter.Tk()
 window.title("tonights the night")
 window.resizable(False, False)
 
-
-##the lines above are code online to make the window, after this it is our work
 canvas = tkinter.Canvas(window, bg = "navy blue", width = WINDOW_WIDTH, height = WINDOW_HEIGHT)
 canvas.pack()
 window.update()
+
 pygame.mixer.init()
 pygame.mixer.music.load("Dexter.mp3")
 pygame.mixer.music.set_volume(0.7)
